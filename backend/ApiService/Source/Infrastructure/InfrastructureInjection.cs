b9c31fadbdf756eefb22fde9927bf759ba7c497a
vovazb <vovazb@connect.net.il>
2025-11-05 17:53:39 +0200

    homework 1 & 2

--- backend/ApiService/Source/Infrastructure/InfrastructureInjection.cs
+++ backend/ApiService/Source/Infrastructure/InfrastructureInjection.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿ using System.Diagnostics.CodeAnalysis;
 using Epam.ItMarathon.ApiService.Domain.Abstract;
 using Epam.ItMarathon.ApiService.Infrastructure.Database;
 using Epam.ItMarathon.ApiService.Infrastructure.Database.Models.AutoMapper;
